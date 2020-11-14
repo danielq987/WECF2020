55c62bfd1e55e47363936409217c931e09e25cbd
danielq987 <danielq987@gmail.com>
2020-11-14 10:24:08 -0500

    Create classes.py

---
+++ classes.py
@@ -0,0 +1,13 @@
+# contains the robot (and map) classes
+
+class robot:
+  def __init__(self):
+    pass
+  # TODO
+
+class map:
+  def __init__(self, length, height):
+    self.length = length
+    self.height = height 
+  # TODO
+        
