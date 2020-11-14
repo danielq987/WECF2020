9694c7fa775dd4bbcdcff83766c78e3640e72ff4
danielq987 <danielq987@gmail.com>
2020-11-14 10:46:48 -0500

    updated main.py

--- src/Map.py
+++ src/Map.py
@@ -1,4 +1,4 @@
-class map:
+class Map:
   def __init__(self, length, height):
     self.length = length
     self.height = height 
--- src/main.py
+++ src/main.py
@@ -1,2 +1,10 @@
-from .Map import map
-from .Robot import Robot
+from Map import Map
+from Robot import Robot
+
+def main():
+  print("hello world")
+  # a = Map(1, 2)
+  # print(Map)
+  
+if __name__ == "__main__":
+  main()
