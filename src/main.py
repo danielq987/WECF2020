b48f971c245bae460a3e670045e3df06255b6f43
Jesse Xia <68565085+x-jesse@users.noreply.github.com>
2020-11-14 10:59:57 -0500

    Add file I/O

--- src/main.py
+++ src/main.py
@@ -2,15 +2,17 @@ from Map import Map
 from Robot import Robot
 
 def main():
-  case1 = open("../test_cases/case1.txt")
-  clean_capacity = case1.read()
-  fuel_capacity = case1.read()
+    case1 = open("../test_cases/case1.txt")  # open test cases and reads values
+    clean_capacity = case1.read()
+    fuel_capacity = case1.read()
 
-  r = Robot(fuel_capacity, clean_capacity)
-  print(r)
+    r = Robot(fuel_capacity, clean_capacity)  # initialize robot
+    print(r)
 
-  a = Map(1, 2)
-  print(Map)
-  
+    rows = case1.read()
+    cols = case1.read()
+    a = Map(rows, cols)  # initialize map
+    print(Map)
+
 if __name__ == "__main__":
-  main()
+    main()
