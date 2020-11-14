1d018f9ded1c80963fc93588639e6e590963a025
danielq987 <danielq987@gmail.com>
2020-11-14 11:11:15 -0500

    Update Map.py

--- src/Map.py
+++ src/Map.py
@@ -15,11 +15,20 @@ class Map:
     """
     return sum([sum(i) for i in self.contamination])
   
+  def __str__(self):
+    string = ""
+    for i in self.contamination:
+      for j in i:
+        string += str(j) + " "
+      string += "\n"
+    return string
+  
 
 def main():
-  contam = [[50, 55]]
+  contam = [[50, 55, 100], [1, 2, 3]]
   map1 = Map(1, 2, contam)
   print(map1.remaining_contamination())
+  print(map1)
   
 if __name__ == "__main__":
   main()
