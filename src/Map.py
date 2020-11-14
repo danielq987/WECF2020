3c721ac5f14f2b46529e51f608d717bbb36428ca
danielq987 <danielq987@gmail.com>
2020-11-14 12:01:34 -0500

    Update Map.py

--- src/Map.py
+++ src/Map.py
@@ -1,15 +1,15 @@
 class Map:
-  def __init__(self, height, length, contamination):
+  def __init__(self, contamination):
     """
-    length = map length, or number of columns
-    height = map height, or number of rows
+    columns = map columns, or number of columns
+    rows = map rows, or number of rows
     contamination is a 2-D list of contamination values
     """
-    self.height = height + 2
-    self.length = length + 2
+    self.rows = len(contamination) + 2
+    self.columns = len(contamination[0]) + 2
     tempList = contamination
-    tempList.insert(0, [0] * length)
-    tempList.append([0] * length)
+    tempList.insert(0, [0] * (self.columns - 2))
+    tempList.append([0] * (self.columns - 2))
     for i in range(len(tempList)):
       l = tempList[i]
       l.insert(0, 0)
@@ -21,7 +21,7 @@ class Map:
     """
     Adds a base at the given coordinates
     """
-    if (col != 0 and col != (self.length - 1)) and (row != 0 and row != (self.height - 1)):
+    if (col != 0 and col != (self.columns - 1)) and (row != 0 and row != (self.rows - 1)):
       raise ValueError(f"Cannot place a base at {row}, {col}")
     self.contamination[row][col] = 'B'
     return None
@@ -31,6 +31,18 @@ class Map:
     Print the sum of all remaining contamination values
     """
     return sum([sum(i) for i in self.contamination])
+  
+  def remaining_tiles(self):
+    """
+    Print the coordinates of tiles which still need to be cleaned
+    Returns a list a tuples (coordinates)
+    """
+    remaining = []
+    for i in range(1, self.rows - 1):
+      for j in range(1, self.columns - 1):
+        if self.contamination[i][j] != 0:
+          remaining.append((i, j))
+    return remaining
   
   def __str__(self):
     """
@@ -45,10 +57,13 @@ class Map:
   
 
 def main():
-  contam = [[50, 55, 100], [1, 2, 3]]
-  map1 = Map(1, 2, contam)
+  contam = [[50, 55, 0], [1, 2, 3]]
+  map1 = Map(contam)
+  print(map1.rows)
+  print(map1.columns)
   print(map1.remaining_contamination())
-  map1.add_base(1,1)
+  map1.add_base(1,0)
+  print(map1.remaining_tiles())
   print(map1)
   
 if __name__ == "__main__":
