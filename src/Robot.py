52257d77589f8d8a994b68dde4e86663c681f228
kiransuren <kirank.suren@gmail.com>
2020-11-14 11:39:59 -0500

    add members to Robot class

--- src/Robot.py
+++ src/Robot.py
@@ -1,14 +1,30 @@
 class Robot:
-  def __init__(self, fuel_capacity, clean_capacity, position_x, position_y, base_x, base_y):
-      self.fuel_cap = fuel_capacity
-      self.clean_cap = clean_capacity
-      self.pos_x = position_x
-      self.pos_y = position_y
-      self.base_x = base_x
-      self.base_y = base_y
+    
+    def __init__(self, name, fuel_capacity, clean_capacity, position_x, position_y, base_x, base_y, route_x, route_y, on_route):
+        self.fuel_cap = fuel_capacity
+        self.clean_cap = clean_capacity
+        self.pos_x = position_x
+        self.pos_y = position_y
+        self.base_x = base_x
+        self.base_y = base_y
+        self.on_route = on_route
+        self.history = [[name, [base_x,base_y]]] # Holds history of moves
+        self.route_x = route_x
+        self.route_y = route_y
+
+    def find_next_move(map_array):
+        i = pos_x
+        j = pos_y
+        
+        #Loop through list of possible next locations
+        # -plan of attack array
+        #if no possible locations, go home
+
+        #Find shortest distance location
+
+        #if the dist is more than fuel
+        #go back home
+
+        
 
 
-
-x = Robot(10, 15)
-
-print(x.clean_cap)
