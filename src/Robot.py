a21a6511e2588a6b494ed52c14d2027fae11b62e
kiransuren <kirank.suren@gmail.com>
2020-11-14 10:35:20 -0500

    add map and robot class template and gitignore

---
+++ src/Robot.py
@@ -0,0 +1,10 @@
+class Robot:
+  def __init__(self, fuel_capacity, clean_capacity):
+      self.fuel_cap = fuel_capacity
+      self.clean_cap = clean_capacity
+
+
+
+x = Robot(10, 15)
+
+print(x.clean_cap)
