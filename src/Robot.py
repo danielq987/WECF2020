8a5c8e82deb99b94277a63a6e421762246721035
kiransuren <kirank.suren@gmail.com>
2020-11-14 11:03:03 -0500

    add class members to Robot class

--- src/Robot.py
+++ src/Robot.py
@@ -1,7 +1,11 @@
 class Robot:
-  def __init__(self, fuel_capacity, clean_capacity):
+  def __init__(self, fuel_capacity, clean_capacity, position_x, position_y, base_x, base_y):
       self.fuel_cap = fuel_capacity
       self.clean_cap = clean_capacity
+      self.pos_x = position_x
+      self.pos_y = position_y
+      self.base_x = base_x
+      self.base_y = base_y
 
 
 
