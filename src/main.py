d8867244e9370f802cc56e29a02e751638d0f208
Jesse Xia <68565085+x-jesse@users.noreply.github.com>
2020-11-14 10:57:13 -0500

    Add file I/O

--- src/main.py
+++ src/main.py
@@ -2,9 +2,15 @@ from Map import Map
 from Robot import Robot
 
 def main():
-  print("hello world")
-  # a = Map(1, 2)
-  # print(Map)
+  case1 = open("../test_cases/case1.txt")
+  clean_capacity = case1.read()
+  fuel_capacity = case1.read()
+
+  r = Robot(fuel_capacity, clean_capacity)
+  print(r)
+
+  a = Map(1, 2)
+  print(Map)
   
 if __name__ == "__main__":
   main()
